063d9c355525b812abe206e4087334e16ad35683
AbdulRehman-cell <abdulrehmanishaque32@gmail.com>
2025-10-23 15:16:32 +0500

    token

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ import base64
 load_dotenv()
 
 # Telegram Bot
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN","")
 
 # Real Turnitin Credentials
 TURNITIN_USERNAME = os.getenv("TURNITIN_USERNAME", "Abiflow")
